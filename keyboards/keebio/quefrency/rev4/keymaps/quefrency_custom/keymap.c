dd79ee77e8538cbf363f910d797ea1840887124d
Alastair Choo <alastairchoo@yahoo.com>
2025-07-21 23:37:26 +0100

    add quick nav

--- keyboards/keebio/quefrency/rev4/keymaps/quefrency_custom/keymap.c
+++ keyboards/keebio/quefrency/rev4/keymaps/quefrency_custom/keymap.c
@@ -1,11 +1,6 @@
 // Copyright 2022 Danny Nguyen (@nooges)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <stdint.h>
-#include "color.h"
-#include "eeconfig.h"
-#include "keycodes.h"
-#include "rgblight.h"
 #include QMK_KEYBOARD_H
 
 /** NOTE
@@ -14,8 +9,9 @@
 
 enum custom_layer {
     _BASE,
-    _FN1,
+    _QUICK,
     _RGB,
+    _FN1,
 };
 
 enum custom_keycodes {
@@ -24,31 +20,34 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    KC_MUTE, RGB_TOG, KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_MPLY,
-    KC_F1,   KC_F2,   KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
-    KC_F3,   KC_F4,   KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
-    KC_F5,   KC_F6,   KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    KC_F7,   KC_F8,   KC_LCTL, MO(_FN1), KC_LOPT, KC_LCMD, KC_SPC,           KC_RCMD, KC_RCMD, MO(_FN1),KC_RCTL, _______, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_MUTE, RGB_TOG, KC_ESC,         KC_1,     KC_2,    KC_3,    KC_4,                KC_5,    KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_MPLY,
+    KC_F1,   KC_F2,   KC_TAB,         KC_Q,     KC_W,    KC_E,    KC_R,                KC_T,    KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+    KC_F3,   KC_F4,   CTL_T(KC_CAPS), KC_A,     KC_S,    KC_D,    KC_F,                KC_G,    KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
+    KC_F5,   KC_F6,   KC_LSFT,        KC_Z,     KC_X,    KC_C,    KC_V,                KC_B,    KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    KC_F7,   KC_F8,   KC_LCTL,        TG(_FN1), KC_LOPT, KC_LCMD, LT(_QUICK, KC_SPC),           _______, MO(_QUICK), KC_RCMD, KC_RCTL, _______, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [_FN1] = LAYOUT_65_with_macro(
+  [_QUICK] = LAYOUT_65_with_macro(
     _______, RGB_MOD, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______,
-    _______, _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_END,
-    _______, _______, EE_CLR,  _______, _______, _______, _______,          _______, _______, _______, EE_CLR, _______, _______, _______, _______
+    _______, _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
+    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, _______, KC_HOME,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, KC_END,
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_RGB] = LAYOUT_65_with_macro(
     _______, RGB_MOD, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______,
-    _______, _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_END,
     _______, _______, EE_CLR,  _______, _______, _______, _______,          _______, _______, _______, EE_CLR, _______, _______, _______, _______
   ),
+
+//   [_FN1]
 };
 
 /* TODO - CURRENT TASK: CHANGE COLOUR DEPENDING ON OS
+
 1) OS detection: the keymap should change depending on the OS
     [x] change colour depending on the OS (Apple: white, Windows: blue, linux: green)
         [ ] test that os colour does change depending on where it's plugged (laptop, PC, raspberry pi(?))
@@ -58,8 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [ ] make chandelure lead into the RGB layer.
     [ ] test chandelure leads into RGB context
     [ ] get the knobs to change different things. HSV? Brightness?
-3) Alternative keybinds: swap ctrl with caps lock, etc.
+3) [x] Alternative keybinds: swap ctrl with caps lock, etc.
 4) Media control layer?
+5) application specifc layers (vscode, fcx, logic, etc)
+
 */
 
 void eeconfig_init_user(void) {
